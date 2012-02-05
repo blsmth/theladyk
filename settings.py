5b0b87f1a01fbeb2a2a963bd4f9376abc2cfc650
Brendan Smith <brendan@thronegroup.com>
2012-02-05 13:43:58 -0500

    don't know why these images are getting 404

--- settings.py
+++ settings.py
@@ -5,8 +5,6 @@ gettext = lambda s: s
 
 PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
     # ('Brendan Smith', 'brendan@thronegroup.com'),
@@ -45,7 +43,7 @@ USE_L10N = True
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/"
 MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, '../static')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
